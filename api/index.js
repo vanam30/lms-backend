9a7fd85108a19a4df14bf28222abf374c8a90ffe
vanam30 <sharmamanav34568@gmail.com>
2025-07-20 18:28:48 +0530

    minor fixes

--- api/index.js
+++ api/index.js
@@ -28,6 +28,13 @@ app.use(async (req, res, next) => {
   await ensureDB();
   next();
 });
+app.get('/', (req, res) => res.json({ status: "OK" }));
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/progress', progressRoutes);
